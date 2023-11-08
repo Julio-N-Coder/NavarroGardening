0abe1ac00316cc1678a9f019247b4f1bf07d4cdd
Julio <Julio.Navarro.Coder@gmail.com>
2023-11-07 16:50:41 -0800

    Finished the Header, Footer, Home page, and About Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function About() {
   return (
-    <h1>About</h1>
+    <main className={styles.background}>
+      <Image className={styles.imgBackground} src='/eco-warrior-princess-TsOeGUwWzWo-unsplash.jpg' layout='fill' alt='Gardening Image' />
+      <div className={styles.backgroundOverlay}></div>
+      <main className={styles.main}>
+        <h1>About Navarro Gardening</h1>
+        <article className={styles.text}><span className={styles.textLine}>Welcome to Navarro Gardening, your local landscaping experts. We are an Individual-owned and operated business based in San Miguel. With over 12 years of experience in the landscaping industry, we take pride in providing top-quality services to our community.</span>
+        <br />
+        <span className={styles.textLine}>Our founder, Cesar, started Navarro Gardening with a simple mission: to create beautiful, sustainable outdoor spaces that enhance our clients&apos; quality of life. Over the years, we have grown into a full-service landscaping company, offering a wide range of services from lawn mowing and hedge trimming to irrigation and yard clean-ups.</span>
+        <br />
+        <span className={styles.textLine}>At Navarro Gardening, we believe that every outdoor space has the potential to be a sanctuary, a place where you can relax, entertain, and enjoy the beauty of nature. Our experienced professional is dedicated to making this vision a reality for our clients.</span>
+        <br />
+        <span className={styles.textLine}>We understand that every yard is unique, which is why we tailor our services to meet the specific needs and preferences of each client. Whether you need regular maintenance services or a complete yard makeover, we&apos;re here to help.</span>
+        <br />
+        <span className={styles.textLine}>Thank you for considering Navarro Gardening for your landscaping needs. We look forward to the opportunity to serve you.</span></article>
+      </main>
+    </main>
   )
 }
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from './header.module.css'
+import logo from '@/../public/navarro-gardening-logo-croped-small.jpg'
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.titleLogo}>
         <h1 className={styles.title}>Navarro Gardening</h1>
-        <Image className={styles.logo} src='/navarro-gardening-logo-croped-small.jpg' width={100} height={100} alt='Navarro Gardening Logo' />
+        <Link href='/' className={styles.logoContainer}>
+        <Image className={styles.logo} src={logo} placeholder='blur' width={100} height={100} alt='Navarro Gardening Logo' />
+        </Link>
         {/* <label className={styles.logo}>Place logo here</label> */}
       </div>
       <nav className={styles.navbar}>
